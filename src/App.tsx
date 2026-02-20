1c61c65ec5339b255ec5b8676bc2e8d6442f73ed
[dyad] <guilherme@framebranding.com.br>
2026-02-20 14:39:18 -0300

    [dyad] Corrigindo erro de sintaxe no App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
                   <Index />
                 </ProtectedRoute>
               } 
-            )
+            />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
